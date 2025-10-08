a3b9b38272f03644dd7e57d1d7a58c8536c17b3d
Juli Abdurahman <JULIABDURRAHMAN02@GMAIL.COM>
2025-10-08 16:45:09 +0700

    Update Server_udp.py

--- Server_udp.py
+++ Server_udp.py
@@ -1,1 +1,28 @@
-uji lqgi sm3
+import socket
+
+
+
+myp = socket.SOCK_DGRAM 
+
+afn = socket.AF_INET
+
+socketServer = socket.socket(afn, myp)
+
+
+
+socketServer.bind(("127.0.0.1", 12345))
+
+print("Server mendengarkan...")
+
+
+
+while True:
+
+    clientData = socketServer.recvfrom(1024)
+
+    addrs = clientData[1][0]
+
+    msg = clientData[0].decode()
+
+    print(addrs + " : " + msg)
+
