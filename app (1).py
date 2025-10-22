eb4c345a9e2fd78a7920fd95fb8dca7d36a78a5c
Juli Abdurahman <JULIABDURRAHMAN02@GMAIL.COM>
2025-10-22 21:41:26 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -1,75 +1,30 @@
-#request -> perintah http -> GET, POST, PUT, DELETE
 from flask import Flask, jsonify, request
-import json
 
-#membuat instance / object bernama app
 app = Flask(__name__)
 
-#perintah baca data
-def ambil_data():
-    with open('ppanti.json', 'r') as file:
-        daftar_anak = json.load(file)
-    return daftar_anak
+data_panti = [
+    {"nomor_induk_panti": 1, "nama": "Rahman", "donasi": "Beras"},
+    {"nomor_induk_panti": 2, "nama": "Irham", "donasi": "Baju"},
+    {"nomor_induk_panti": 3, "nama": "Ibnu", "donasi": "Uang"},
+]
 
-#perintah simpan data
-def simpan_data(data):
-    with open('ppanti.json', 'w') as file:
-        json.dump(data, file, indent=4)
+@app.route('/data', methods=['GET'])
+def tampil_data():
+    return jsonify(data_panti)
 
-#------------------------------------ROUTES--------------------------------------
-@app.route('/') #rute homepage
-def home(): #function 
-    return '<H1>Selamat Datang Di Panti Asuhan Mutiara Insan</H1>'
+@app.route('/tambah', methods=['POST'])
+def tambah_data():
+    data_baru = request.get_json()
+    data_panti.append(data_baru)
+    return jsonify(data_panti)
 
-#Routes baca data-------------------------------------------------
-@app.route('/baca_data', methods=['GET'])
-def get_data():
-    daftar_anak = ambil_data()
-    return jsonify(daftar_anak)
-
-#Routes simpan data baru-----------------------------------------
-@app.route('/tambah_data', methods=['POST'])
-def penambahan_daftar_anak():
-    data = ambil_data()
-    nomor_induk_panti = request.form.get('nomor_induk_panti')
-    nama = request.form.get('nama')
-    jenis_kelamin = request.form.get('jenis_kelamin')
-
-    yambah_anak = {
-        'nomor_induk_ktp' : nomor_induk_panti,
-        'nama' : nama,
-        'jenis_kelamin' : jenis_kelamin 
-    }
-
-    data.append(tambah_anak)
-    simpan_data(data)
-    return jsonify(data)
-
-#Routes Udah data / Edit-----------------------------------------
-@app.route('/edit/<nomor_induk_panti, methods=['POST'])
-def edit_dafar_anak(nomor_induk_panti):
-    data = ambil_data()
-    for pend in data:
-        if pend['nomor_induk_panti] == nomor_induk_panti: #cari data daftar anak dengan nomor_induk_panti sesuai variabel nomor_induk_panti 
-            pend['nama'] = request.form.get('nama')
-            pend['gender'] = request.form.get('jenis_kelamin)
-            simpan_data(data)
-            return 'Data berhasil diubah'
-    return 'Data tidak ditemukan'
-    
-
-#Routes Hapus data ----------------------------------------------
 @app.route('/hapus/<nomor_induk_panti>', methods=['DELETE'])
-def delete_data(nomor_induk_panti):
-    data = ambil_data()
-    for pend in data:
-        if pend['nomor_induk_panti] == nomor_induk_panti:
-            data.remove(pend)
+def hapus_data(nomor_induk_panti):
+    for d in data_panti:
+        if d["nomor_induk_panti"] == int(nomor_induk_panti):
+            data_panti.remove(d)
             break
+    return jsonify(data_panti)
 
-    simpan_data(data)
-    return 'Data berhasil dihapus'
-
-#menjalankan file app.py
 if __name__ == '__main__':
     app.run(debug=True)
